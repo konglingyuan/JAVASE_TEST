ea7ca5e7612a9046030ede2f7e4d14028ec51670
konglingyuan <konglingy_lady@sina.com>
2019-07-31 14:42:28 +0800

    第一次提交，测试git使用方式

---
+++ src/com/konglingyuan/test01/test01.java
@@ -0,0 +1,12 @@
+package com.konglingyuan.test01;
+
+public class test01 {
+
+    public static void main(String[] args) {
+        System.out.println("直接输入main  就能调用main方法");
+        System.out.println("class文件是编译后文件");
+        System.out.println("代码要进行实时保存");
+        System.out.println("源文件  目标文件");
+        System.out.println("ceshi");
+    }
+}
